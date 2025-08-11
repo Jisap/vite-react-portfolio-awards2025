9522a34d296c9ca6ed8bd84618a3556cb60b44de
Jorge Matos <jisapjisap@gmail.com>
2025-08-11 14:34:45 +0200

    Add ServiceSummary component and integrate into App
    - Created a new ServiceSummary component to display service offerings with GSAP animations.
    - Integrated ServiceSummary into the App component, enhancing the layout with additional sections for improved structure.
    - Removed duplicate Tailwind CSS import from index.css for cleaner code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+
 import Hero from "./sections/Hero"
 import Navbar from "./sections/Navbar"
+import ServiceSummary from "./sections/ServiceSummary"
+
+
 
 const App = () => {
+
   return (
     <div className="relative w-screen min-h-screen overflow-x-auto">
       <Navbar />
       <Hero />
+      <ServiceSummary />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
     </div>
   )
 }
